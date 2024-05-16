1ee87531e714a0e333adef66e975b962482bc5a9
savannahmarshall <savvymarshall@gmail.com>
2024-05-16 14:03:15 -0700

    started building code for localStorage in script.js and added modal in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,61 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// const tasks = document.getElementById("#formModal");
+
+// const task = document.getElementById("#task-title");
+// const dueDate = document.getElementById("#task-due-date");
+// const taskDescription = document.getElementById("task-description");
+
+// const taskList = [];
+
+let task = {
+    title: taskName,
+    dueDate: taskDueDate,
+    description: taskDescription,
+    uniqueId: taskId,
+    status: 'to-do',
+}
+
+
+
+
+
+function getFromLocalStorage() {
+    let taskList = JSON.parse(localStorage.getItem('tasks'));
+    if (!taskList) {
+        taskList = [];
+    }
+    return taskList;
+
+}
+
+function saveInLocalStorage(taskList) {
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+}
+
+function readIdFromLocalStorage() {
+    let nextId = JSON.parse(localStorage.getItem('nextId'));
+    if(!nextId) {
+        nextId = [];
+    }
+    return nextId;
+}
+
+function saveIdToLocalStorage() {
+    localStorage.setItem('nextId', JSON.stringify(nextId));
+}
+
+
+
+
+
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
-}
+    let id = "id" + Math.random().toString(16).slice(2)
+    console.log(id)
+}     
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
@@ -34,5 +84,23 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+
+
+    addTaskButton.addEventListener('click', function(event) {
+        event.preventDefault();
+    
+    });
+
+
+
+// Datepicker widget for due date
+$(function () {
+    $('#task-due-date').datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
+
+  
 
 });
